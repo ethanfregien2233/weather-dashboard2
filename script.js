be81ef8b3f88e6efdccb604ffa283b66e36b48e3
ethan fregien <ethanfregien@gmail.com>
2021-10-07 17:35:51 -0500

    more js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
+var cityInput=""; 
+var queryUrl="";
 
+storeCities();  
 
-var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
-    "abf8999e7d50163f5a2249b31d6680c5";
+ $("#citySelect").on("click", function(event) {
+    event.preventDefault();
+    cityInput = $("#input").val();
+    console.log(citySelect, "citySelect")
+    }); 
 
+    
+    function storeCities () {
 
-$.ajax({
-    url: queryUrl,
-    method: "GET",
-}).then(function(response) {
-    console.log(response);
-    for (var i = 0; i < response.data.length; i++) {
-        console.log("TITLE", response.data[i]. titile);
-    }
+     
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" 
+    + cityInput + "&appid=" + "abf8999e7d50163f5a2249b31d6680c5";  
+    console.log(queryUrl);
+
+     
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+        console.log(response.city)
+         })
+        } 
+
+    
+    $("#dWeather").append(
+
+    "<div class='col s12 m6'>"
+ 
+    +  "<h2>" + response.city + "</h2>"
+
+    
+    + "</div>"
+    );  
+
 });
-});
-
